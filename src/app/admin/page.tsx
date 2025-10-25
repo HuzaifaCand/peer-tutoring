d376e0ed381cd2e1bb1b710a1a4cb5808a5b3795
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-10-26 02:15:37 +0500

    overview setup here

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,9 @@
+import OverviewPage from "@/components/admin/overview/OverviewPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "PeerLink | Admin Dashboard",
+};
 export default function AdminPage() {
-  return <></>;
+  return <OverviewPage />;
 }
