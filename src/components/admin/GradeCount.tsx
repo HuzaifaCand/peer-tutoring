076e66090556585fda75a23c70f1ded7f88e5a00
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-10-28 21:52:43 +0500

    also hook loc change

--- src/components/admin/GradeCount.tsx
+++ src/components/admin/GradeCount.tsx
@@ -1,63 +1,19 @@
 "use client";
 
 import CountCard from "@/components/cards/CountCard";
-import { supabase } from "@/lib/supabase/client";
-import { useEffect, useState } from "react";
+import { useGradeCount } from "@/hooks/admin/useGradeCount";
 
-export default function GradeCount({ type }: { type: "tutors" | "students" }) {
-  const [counts, setCounts] = useState({
-    as: 0,
-    a2: 0,
-  });
-
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    async function fetchCounts() {
-      try {
-        setLoading(true);
-
-        const [as, a2] = await Promise.all([
-          supabase
-            .from(type)
-            .select("*", { count: "exact", head: true })
-            .eq("grade", "AS"),
-          supabase
-            .from(type)
-            .select("*", { count: "exact", head: true })
-            .eq("grade", "A2"),
-        ]);
-
-        if (as.error || a2.error) {
-          throw new Error(as.error?.message || a2.error?.message);
-        }
-
-        setCounts({
-          as: as.count || 0,
-          a2: a2.count || 0,
-        });
-      } catch (err) {
-        console.error(`Error fetching ${type} counts:`, err);
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    fetchCounts();
-  }, []);
+export default function GradeCountCards({
+  type,
+}: {
+  type: "tutors" | "students";
+}) {
+  const { loading, counts } = useGradeCount(type);
 
   return (
     <div className="grid grid-cols-2 gap-4">
-      <CountCard
-        loading={loading}
-        count={counts.as}
-        label={`AS ${type.charAt(0).toUpperCase() + type.slice(1)}`}
-      />
-      <CountCard
-        loading={loading}
-        count={counts.a2}
-        label={`A2 ${type.charAt(0).toUpperCase() + type.slice(1)}`}
-      />
+      <CountCard loading={loading} count={counts.as} label={`In AS`} />
+      <CountCard loading={loading} count={counts.a2} label={`In A2`} />
     </div>
   );
 }
