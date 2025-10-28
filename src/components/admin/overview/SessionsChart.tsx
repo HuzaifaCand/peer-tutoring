49611c82190253f7f86e133fce08767674c365bd
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-10-28 21:51:10 +0500

    import location change

--- src/components/admin/overview/SessionsChart.tsx
+++ src/components/admin/overview/SessionsChart.tsx
@@ -2,7 +2,7 @@
 
 import Chart from "@/components/Chart";
 import MonthSelector from "@/components/MonthSelector";
-import { useMonthlySessionData } from "@/hooks/useMonthlySessionData";
+import { useMonthlySessionData } from "@/hooks/admin/useMonthlySessionData";
 import { getMonthLabel } from "@/utils/months";
 
 export default function SessionsChart() {
