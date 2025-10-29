a53a30bd5fad0847f37b29fd8a94cb4f2f5fd5ec
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-10-29 07:59:00 +0500

    copy pasted contents of database.types.ts into databasetypes.ts, should fix the parsing issue

--- src/lib/types/base.ts
+++ src/lib/types/base.ts
@@ -1,6 +1,6 @@
 // --base types -- wont ever change
 
-import { Database } from "../database.types";
+import { Database } from "../databasetypes";
 
 export type StudentRow = Database["public"]["Tables"]["students"]["Row"];
 export type TutorRow = Database["public"]["Tables"]["tutors"]["Row"];
