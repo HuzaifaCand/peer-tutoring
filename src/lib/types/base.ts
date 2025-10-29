48088db441b47bf96cfd44f9cc58e1edab91d510
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-10-29 16:00:24 +0500

    might need to change

--- src/lib/types/base.ts
+++ src/lib/types/base.ts
@@ -6,3 +6,4 @@ export type StudentRow = Database["public"]["Tables"]["students"]["Row"];
 export type TutorRow = Database["public"]["Tables"]["tutors"]["Row"];
 export type SessionRow = Database["public"]["Tables"]["tutors"]["Row"];
 export type UserRow = Database["public"]["Tables"]["users"]["Row"];
+export type SlotsRow = Database["public"]["Tables"]["available_slots"]["Row"];
--- src/lib/types/views.ts
+++ src/lib/types/views.ts
@@ -1,4 +1,4 @@
-import { StudentRow, TutorRow, UserRow } from "./base";
+import { StudentRow, TutorRow, UserRow, SlotsRow } from "./base";
 
 // formed types, full comemnting needed to know where they are going as i dont want to keep them in the same file but i probably should idk
 
@@ -6,22 +6,7 @@ export type StudentUser = StudentRow & {
   users: Pick<UserRow, "full_name" | "email">; // student table on admins/student
 };
 
-export const studentUserColumns = [
-  { key: "id", label: "ID", width: 80 },
-  { key: "full_name", label: "Name", width: 150, truncate: true },
-  { key: "email", label: "Email", width: 200, truncate: true },
-  { key: "grade", label: "Grade", width: 80 },
-  { key: "subjects", label: "Subjects", width: 150 },
-];
-
 export type TutorUser = TutorRow & {
   users: Pick<UserRow, "full_name" | "email">; // student table on admins/student
+  avaialble_slots: SlotsRow[];
 };
-
-export const tutorUserColumns = [
-  { key: "id", label: "ID", width: 80 },
-  { key: "full_name", label: "Name", width: 150, truncate: true },
-  { key: "email", label: "Email", width: 200, truncate: true },
-  { key: "grade", label: "Grade", width: 80 },
-  { key: "subjects", label: "Subjects", width: 150 },
-];
