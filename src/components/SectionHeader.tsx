149a70b2fa04f3352dad5e2fdbe4dc2a6a58227d
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-10-30 16:49:20 +0500

    proper text color in here

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -6,7 +6,7 @@ export default function SectionHeader({
   rightSlot?: React.ReactNode;
 }) {
   return (
-    <div className="pb-4 border-b border-textWhite/10 flex flex-col gap-2 sm:gap-0 sm:flex-row sm:justify-between sm:items-end mb-8">
+    <div className="pb-4 border-b border-textWhite/10 flex text-textWhite flex-col gap-2 sm:gap-0 sm:flex-row sm:justify-between sm:items-end mb-8">
       <h1 className="text-2xl md:text-3xl font-semibold">{title}</h1>
       {rightSlot && (
         <div className="text-xs md:text-sm text-textMuted">{rightSlot}</div>
