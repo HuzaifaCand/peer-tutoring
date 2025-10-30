16d168dbca810dfe1abab450f6afb00bfd7f15a2
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-10-30 13:18:15 +0500

    computed table types instead of unknown

--- src/components/table/types.ts
+++ src/components/table/types.ts
@@ -1,11 +1,13 @@
 import { Dispatch, SetStateAction } from "react";
+import { ComputedTutorRow } from "../admin/tutors/getTutors";
+import { ComputedStudentRow } from "../admin/students/getStudents";
 
 export interface TableColumn<T> {
   key: keyof T | string;
   label: string;
   width?: string | number;
   truncate?: boolean;
-  render?: (row: T) => React.ReactNode; // ✅ NEW: custom render logic
+  render?: (row: T) => React.ReactNode; // custom render logic
 }
 
 export interface TableRowProps<T> {
@@ -13,6 +15,11 @@ export interface TableRowProps<T> {
   columns: TableColumn<T>[];
   onClick?: () => void;
 }
+
+export type TableRowByType = {
+  tutors: ComputedTutorRow;
+  students: ComputedStudentRow;
+};
 
 export type refetchFlagType = Dispatch<SetStateAction<boolean>>;
 export interface DataTableProps<T> {
@@ -23,6 +30,7 @@ export interface DataTableProps<T> {
   type?: "students" | "tutors" | "sessions";
   loading: boolean;
   setRefetchFlag: refetchFlagType;
+  setRowCount: (rows: number) => void;
 }
 
 export interface TableLoadingProps<T> {
