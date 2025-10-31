dcbf7d76c4b69cba4a2ba1798d2cd14e353524b6
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-10-31 22:09:30 +0500

    badge actually useful now

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -4,14 +4,24 @@ interface BadgeProps {
   title?: string;
 }
 
+function capitalize(s: string) {
+  return s.slice(0, 1).toUpperCase() + s.slice(1);
+}
+
 export default function Badge({ value, styles, title }: BadgeProps) {
+  const capitalizedValue = capitalize(value);
+  const formattedValue = value.includes("-")
+    ? capitalizedValue.split("-")[0] +
+      " " +
+      capitalize(capitalizedValue.split("-")[1])
+    : capitalizedValue;
   return (
     <span
       title={title}
       className={`inline-flex items-center justify-center px-2 py-[2px] mt-0.5 rounded-md 
                   text-[10px] font-medium leading-none ${styles}`}
     >
-      {value}
+      {formattedValue}
     </span>
   );
 }
