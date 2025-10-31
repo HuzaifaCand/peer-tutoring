911540407850da655718a6da93e8a692eed5de87
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-10-31 08:07:18 +0500

    text color

--- src/components/cards/CountCard.tsx
+++ src/components/cards/CountCard.tsx
@@ -10,7 +10,7 @@ export default function CountCard({
   loading?: boolean;
 }) {
   return (
-    <div className="bg-elevatedBg rounded-xl p-4 sm:p-6 flex flex-col items-start justify-between">
+    <div className="bg-elevatedBg text-textWhite rounded-xl p-4 sm:p-6 flex flex-col items-start justify-between">
       <p className="text-sm text-textMuted">{label}</p>
       <div className="mt-1 sm:mt-2 h-[32px] flex items-center">
         {loading ? (
