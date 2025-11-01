f047cc5e943a6e516fc2e4ad205d71abab4b586f
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-01 10:44:17 +0500

    better > sm width

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -7,7 +7,7 @@ export default function SectionHeader({
 }) {
   return (
     <div className="pb-4 border-b border-textWhite/10 flex text-textWhite flex-col gap-2 sm:gap-0 sm:flex-row sm:justify-between sm:items-end mb-8">
-      <h1 className="text-3xl font-semibold">{title}</h1>
+      <h1 className="text-[1.6875rem] sm:text-3xl font-semibold">{title}</h1>
       {rightSlot && (
         <div className="text-xs md:text-sm text-textMuted">{rightSlot}</div>
       )}
