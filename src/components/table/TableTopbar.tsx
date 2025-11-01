a39f7835c86d3f37f73207ace8b536cb0744ca6f
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-01 10:57:44 +0500

    topbar scrollable attempt

--- src/components/table/TableTopbar.tsx
+++ src/components/table/TableTopbar.tsx
@@ -45,18 +45,20 @@ export function TableTopbar({
   };
 
   return (
-    <div className="flex justify-between items-center mb-3">
-      <div className="flex items-center gap-3">
+    <div className="relative flex justify-between items-center mb-3 overflow-x-auto no-scrollbar">
+      <div className="flex items-center gap-3 flex-shrink-0">
         {gradeCounts && <GradeCounts counts={gradeCounts} loading={loading} />}
         {type === "cancelledSessions" && (
-          <p className="text-xs text-textMuted">{rowCount} rows shown</p>
+          <p className="text-xs text-textMuted whitespace-nowrap">
+            {rowCount} rows shown
+          </p>
         )}
         {healthFilter && type === "subjects" && (
           <SubjectHealthFilter healths={healthFilter} />
         )}
       </div>
 
-      <div className="flex items-center gap-2 mb-1">
+      <div className="flex items-center gap-2 flex-shrink-0 ml-auto">
         {availabilityFilter && type === "tutors" && (
           <AvailabilityFilter availabilityFilter={availabilityFilter} />
         )}
@@ -64,8 +66,8 @@ export function TableTopbar({
         {searchable && (
           <div className="flex items-center gap-2">
             {type.includes("Sessions") && (
-              <div title="You can search by tutor or student id as well">
-                <Info className="w-5 h-5 text-textMuted/80 hover:text-textWhite" />
+              <div title="You can search by tutor or student ID as well">
+                <Info className="w-5 h-5 text-textMuted/80 hover:text-textWhite shrink-0" />
               </div>
             )}
             <TableSearch value={value} onChange={setValue} />
@@ -76,12 +78,11 @@ export function TableTopbar({
           onClick={handleRefresh}
           title="Refresh data"
           disabled={loading}
-          className={`p-2 rounded-md transition active:scale-95 
-            ${
-              loading
-                ? "bg-elevatedBg/30 cursor-not-allowed opacity-60"
-                : "bg-elevatedBg/50 hover:bg-elevatedBg text-textWhite/90 hover:text-textButton"
-            }`}
+          className={`p-2 rounded-md transition active:scale-95 flex-shrink-0 ${
+            loading
+              ? "bg-elevatedBg/30 cursor-not-allowed opacity-60"
+              : "bg-elevatedBg/50 hover:bg-elevatedBg text-textWhite/90 hover:text-textButton"
+          }`}
         >
           <RefreshCcw
             className={`w-4 h-4 ${
