9b879a32598d81da8d01c8dba3bf691eb6b748f7
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-02 12:04:21 +0500

    bar graph component added and finalized

---
+++ src/components/BarGraph.tsx
@@ -0,0 +1,57 @@
+import { Loader2 } from "lucide-react";
+import {
+  Bar,
+  BarChart,
+  LabelList,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from "recharts";
+import { ComputedSubjectPopularityView } from "./admin/sessions/overview/SubjectsPopularityGraph";
+
+export default function BarGraph({
+  data,
+  loading,
+}: {
+  data: ComputedSubjectPopularityView[];
+  loading: boolean;
+}) {
+  if (loading)
+    return (
+      <div className="flex items-center justify-center h-64">
+        <Loader2 className="animate-spin text-textMuted w-7 h-7" />
+      </div>
+    );
+  return (
+    <div className="w-full overflow-x-auto no-scrollbar">
+      <div className="min-w-[700px]">
+        <ResponsiveContainer width="100%" height={350}>
+          <BarChart margin={{ top: 20 }} data={data}>
+            <XAxis
+              dataKey="subject"
+              tick={{ fill: "rgba(255,255,255,0.6)", fontSize: 11 }}
+              interval={0}
+            />
+            <YAxis
+              width={40}
+              tick={{ fill: "rgba(255,255,255,0.3)", fontSize: 12 }}
+            />
+            <Bar
+              dataKey="session_count"
+              fill="#50d6ffff"
+              radius={[4, 4, 0, 0]}
+              animationDuration={800}
+            >
+              <LabelList
+                dataKey="session_count"
+                position="top"
+                fill="#fff"
+                fontSize={10}
+              />
+            </Bar>
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
+  );
+}
