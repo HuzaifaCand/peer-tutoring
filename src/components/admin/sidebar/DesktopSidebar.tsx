ab47be14858f541f836abbd62cede55d155efc9f
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-03 23:01:07 +0500

    what did i j push?

--- src/components/admin/sidebar/DesktopSidebar.tsx
+++ src/components/admin/sidebar/DesktopSidebar.tsx
@@ -2,7 +2,7 @@ import SidebarContent from "./SidebarContent";
 
 export default function DesktopSidebar() {
   return (
-    <aside className="h-full overflow-y-auto scrollbar-thin scrollbar-track-transparent w-64 bg-,aomNh text-textWhite border-r border-white/5 ">
+    <aside className="h-full overflow-y-auto scrollbar-thin scrollbar-track-transparent w-64 bg-mainBg text-textWhite border-r border-white/5 ">
       <SidebarContent />
     </aside>
   );
