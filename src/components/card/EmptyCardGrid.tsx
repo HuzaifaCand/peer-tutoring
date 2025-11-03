de192e6828c227462c304d33d3bb13c2000303ac
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-03 08:37:28 +0500

    name change

--- src/components/card/EmptyCardGrid.tsx
+++ src/components/card/EmptyCardGrid.tsx
@@ -1,6 +1,6 @@
 import { ListX } from "lucide-react";
 
-export function EmptyTableGrid() {
+export function EmptyGrid() {
   return (
     <div className="h-60 w-full">
       <div className="flex flex-col items-center justify-center gap-2 text-textMuted bg-mainBg border border-elevatedBg/70 rounded-md h-full">
