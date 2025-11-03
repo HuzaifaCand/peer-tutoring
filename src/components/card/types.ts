51167581504719d1a0826db25e2e6dec4c2e6c9e
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-03 19:31:30 +0500

    handle card click added

--- src/components/card/types.ts
+++ src/components/card/types.ts
@@ -1,8 +1,8 @@
-import { ComputedActiveSessionRow } from "../admin/sessions/active/getActiveSessions";
+import { ComputedActiveSession } from "../admin/sessions/active/getActiveSessions";
 import { refetchFlagType } from "../table/types";
 
 export type CardByType = {
-  activeSessions: ComputedActiveSessionRow;
+  activeSession: ComputedActiveSession;
   // later: tutors: ComputedTutorRow, etc.
 };
 
@@ -13,6 +13,7 @@ export interface CardGridProps<K extends keyof CardByType> {
   lastUpdated?: Date;
   layoutClassName?: string;
   emptyMessage?: string;
+  handleCardClick: (item: CardByType[K]) => void;
   getKey?: (item: CardByType[K], index: number) => string | number;
   setRefetchFlag: refetchFlagType;
 }
