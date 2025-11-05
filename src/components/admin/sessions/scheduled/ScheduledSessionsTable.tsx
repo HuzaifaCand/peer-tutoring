5dd849991f4881beb3a8e183376f2808836577ae
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-05 13:54:38 +0500

    Ascending sorting by scheduled_for

--- src/components/admin/sessions/scheduled/ScheduledSessionsTable.tsx
+++ src/components/admin/sessions/scheduled/ScheduledSessionsTable.tsx
@@ -9,13 +9,21 @@ import { scheduledSessionColumns } from "./ScheduledSessionColumns";
 import { useModalOpener } from "@/components/modal/useModalOpener";
 import { useDataFetch } from "@/hooks/useDataFetch";
 import { SessionDataProps } from "../types";
+import { sortByTimestamp } from "@/utils/sortUtils";
 
 export default function ScheduledSessionsTable({
   setCount,
   setSelectedSession,
   setShowModal,
 }: SessionDataProps<ComputedScheduledSessionRow>) {
-  const { data, loading, setRefetchFlag } = useDataFetch(getScheduledSessions);
+  const sortFn = sortByTimestamp<ComputedScheduledSessionRow>(
+    "scheduled_for",
+    "asc"
+  );
+
+  const { data, loading, setRefetchFlag } = useDataFetch(getScheduledSessions, {
+    sortFn,
+  });
 
   const { handleOpen } = useModalOpener(setShowModal, setSelectedSession, "id");
 
