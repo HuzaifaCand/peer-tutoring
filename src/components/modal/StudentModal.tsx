b34873534a76eb4f1ef3c811e5c26c0e09010181
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-06 15:43:09 +0500

    Student and tutor modal init

---
+++ src/components/modal/StudentModal.tsx
@@ -0,0 +1,11 @@
+import { ComputedStudentRow } from "../../lib/users/getStudents";
+
+export function StudentModal({
+  student,
+  onClose,
+}: {
+  student: ComputedStudentRow;
+  onClose: () => void;
+}) {
+  return <div></div>;
+}
---
+++ src/components/modal/TutorModal.tsx
@@ -0,0 +1,11 @@
+import { ComputedTutorRow } from "../../lib/users/getTutors";
+
+export function TutorModal({
+  tutor,
+  onClose,
+}: {
+  tutor: ComputedTutorRow;
+  onClose: () => void;
+}) {
+  return <div></div>;
+}
