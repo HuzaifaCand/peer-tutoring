4941644aaec92c28d0d41050a2cd8e69afa9888d
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-08 11:48:13 +0500

    type updated and very simplified, will be simplified more, might not even exist soon

--- src/lib/sessions/types.ts
+++ src/lib/sessions/types.ts
@@ -1,7 +1,6 @@
 import { Dispatch, SetStateAction } from "react";
 
 export interface SessionDataProps<T> {
-  setShowModal: Dispatch<SetStateAction<boolean>>;
   setSelectedSession: Dispatch<SetStateAction<T | null>>;
   setCount: Dispatch<SetStateAction<number>>;
 }
