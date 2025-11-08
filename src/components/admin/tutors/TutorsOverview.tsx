781d9fcfb31e0322377eafccd07b414667c3d1b4
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-08 14:03:29 +0500

    tutors modal moved lower and fixed closing animation

--- src/components/admin/tutors/TutorsOverview.tsx
+++ src/components/admin/tutors/TutorsOverview.tsx
@@ -2,42 +2,19 @@
 
 import SectionHeader from "@/components/ui/SectionHeader";
 import TutorsTable from "./TutorsTable";
-import { useEffect, useState } from "react";
-import { ComputedTutorRow, getTutorById } from "../../../lib/users/getTutors";
-import { Modal } from "@/components/modal/ModalComponent";
-import { useCloseModal } from "@/components/modal/useCloseModal";
+import { useState } from "react";
 
 export default function TutorsOverview() {
   const [rowCount, setRowCount] = useState(0);
-  const [selectedTutor, setSelectedTutor] = useState<ComputedTutorRow | null>(
-    null
-  );
-
-  const closeModal = useCloseModal(setSelectedTutor);
-
-  useEffect(() => {
-    const url = new URL(window.location.href);
-    const id = url.searchParams.get("id");
-    if (id) {
-      const getTutor = async () => {
-        const data = await getTutorById(id);
-        setSelectedTutor(data);
-      };
-      getTutor();
-    }
-  }, []);
 
   return (
     <main>
-      {selectedTutor && (
-        <Modal type="tutor" onClose={closeModal} data={selectedTutor} />
-      )}
       <SectionHeader
         title="Registered Tutors"
         rightSlot={`${rowCount} rows shown`}
       />
 
-      <TutorsTable setTutor={setSelectedTutor} setRowCount={setRowCount} />
+      <TutorsTable setRowCount={setRowCount} />
     </main>
   );
 }
--- src/components/admin/tutors/TutorsTable.tsx
+++ src/components/admin/tutors/TutorsTable.tsx
@@ -1,39 +1,62 @@
 "use client";
 
 import { Table } from "@/components/table/Table";
-import { ComputedTutorRow, getTutors } from "../../../lib/users/getTutors";
+import {
+  ComputedTutorRow,
+  getTutorById,
+  getTutors,
+} from "../../../lib/users/getTutors";
 import { tutorColumns } from "./TutorTableColumns";
 import { useDataFetch } from "@/hooks/useDataFetch";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { defaultSorters } from "@/utils/sorters";
 import { useModalOpener } from "@/components/modal/useModalOpener";
+import { useCloseModal } from "@/components/modal/useCloseModal";
+import { Modal } from "@/components/modal/ModalComponent";
 
 interface TutorsTableProps {
   setRowCount: Dispatch<SetStateAction<number>>;
-  setTutor: Dispatch<SetStateAction<ComputedTutorRow | null>>;
 }
 
-export default function TutorsTable({
-  setRowCount,
-  setTutor,
-}: TutorsTableProps) {
+export default function TutorsTable({ setRowCount }: TutorsTableProps) {
+  const [selectedTutor, setSelectedTutor] = useState<ComputedTutorRow | null>(
+    null
+  );
+
+  const closeModal = useCloseModal(setSelectedTutor);
+
+  useEffect(() => {
+    const url = new URL(window.location.href);
+    const id = url.searchParams.get("id");
+    if (id) {
+      const getTutor = async () => {
+        const data = await getTutorById(id);
+        setSelectedTutor(data);
+      };
+      getTutor();
+    }
+  }, []);
   const sortFn = defaultSorters.tutor;
 
   const { data, loading, setRefetchFlag } = useDataFetch(getTutors, { sortFn });
 
-  const { handleOpen } = useModalOpener(setTutor);
+  const { handleOpen } = useModalOpener(setSelectedTutor);
 
   const handleClick = (t: ComputedTutorRow) => handleOpen(t);
 
   return (
-    <Table
-      type="tutor"
-      data={data}
-      columns={tutorColumns}
-      loading={loading}
-      setRefetchFlag={setRefetchFlag}
-      setRowCount={setRowCount}
-      onRowClick={handleClick}
-    />
+    <>
+      <Modal type="tutor" onClose={closeModal} data={selectedTutor} />
+
+      <Table
+        type="tutor"
+        data={data}
+        columns={tutorColumns}
+        loading={loading}
+        setRefetchFlag={setRefetchFlag}
+        setRowCount={setRowCount}
+        onRowClick={handleClick}
+      />
+    </>
   );
 }
