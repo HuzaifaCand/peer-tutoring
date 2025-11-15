84b0e48a550971ed5c7d41f72bcb1495ca32e4be
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-15 17:56:27 +0500

    Text size addable

--- src/components/ui/CardCTA.tsx
+++ src/components/ui/CardCTA.tsx
@@ -4,13 +4,19 @@ import { ArrowRight } from "lucide-react";
 interface CTAProps {
   cta: string;
   padding?: string;
+  textSize?: string;
 }
-export function CardCTA({ cta, padding = "py-1 px-3" }: CTAProps) {
+export function CardCTA({
+  cta,
+  padding = "py-1 px-3",
+  textSize = "text-xs",
+}: CTAProps) {
   return (
     <span
       className={clsx(
-        "text-xs flex items-center gap-1 text-textButton border border-white/10 rounded-md  group-hover:bg-hoverBg transition-all",
-        padding
+        "flex items-center gap-1 text-textButton border border-white/10 rounded-md  group-hover:bg-hoverBg transition-all",
+        padding,
+        textSize
       )}
     >
       {cta}{" "}
