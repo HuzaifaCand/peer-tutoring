12387db9cb6254a34e99c10c83ffc09e0062286a
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-15 06:52:31 +0500

    final v of use user subjects

--- src/hooks/useUserSubjects.ts
+++ src/hooks/useUserSubjects.ts
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { useUserRole } from "./useUserRole";
 import { supabase } from "@/lib/supabase/client";
 import { SubjectRow } from "@/lib/computedtypes";
-import { useAuthUser } from "./useAuthUser";
 
 export function useUserSubjects() {
   const [subjects, setSubjects] = useState<
@@ -16,11 +15,9 @@ export function useUserSubjects() {
   useEffect(() => {
     if (!role || role === "admin") return;
     setLoading(true);
-    console.log("new hook running bro :D");
 
     async function fetchSubjects() {
       const table = role === "tutor" ? "tutor_subjects" : "student_subjects";
-      const idType = role === "tutor" ? "tutor_id" : "student_id";
 
       const { data, error } = await supabase
         .from(table)
