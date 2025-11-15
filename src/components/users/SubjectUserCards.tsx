879b8376c65e857c3b93523990c1af34a600f2e0
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-15 13:48:53 +0500

    .

---
+++ src/components/users/SubjectUserCards.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useCallback } from "react";
+import { useDataFetch } from "@/hooks/useDataFetch";
+import { getUsersForSubjects } from "./getSubjectUsers";
+import { CardsLoading } from "../card/CardsLoading";
+import { SubjectUserCard } from "./SubjectUserCard";
+
+export default function SubjectUserCards({
+  uid,
+  role,
+}: {
+  uid: string;
+  role: "tutor" | "student";
+}) {
+  const fetchFn = useCallback(
+    () => getUsersForSubjects(role, uid),
+    [role, uid]
+  );
+  const { data, loading } = useDataFetch(fetchFn);
+
+  if (loading)
+    return <CardsLoading layoutClassName="grid grid-cols-1 gap-4" count={2} />;
+
+  if (!data?.length)
+    return <p className="text-sm text-textMuted">Reload page.</p>;
+
+  return (
+    <section>
+      <div className="sm:mb-4 mb-6 sm:flex sm:items-center sm:gap-3">
+        <h2 className="text-2xl text-textWhite font-semibold">Your Subjects</h2>
+        <p className="text-xs text-textMuted/70 mt-1">
+          You can update your subjects anytime.
+        </p>
+      </div>
+
+      <div className="grid grid-cols-1 gap-4">
+        {data.map((subject) => (
+          <SubjectUserCard
+            key={subject?.subject_id}
+            subject={subject}
+            role={role}
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
