6a461b539c70f1ae800e1aa60a925f5f036f2e53
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-17 18:58:24 +0500

    loading added

--- src/components/users/student/BrowseTutors.tsx
+++ src/components/users/student/BrowseTutors.tsx
@@ -5,6 +5,7 @@ import { subject, useUserSubjects } from "@/hooks/useUserSubjects";
 import { useState, useEffect } from "react";
 import SubjectTabs from "./SubjectTabs";
 import { TutorCards } from "./TutorCards";
+import Loading from "@/components/Loading";
 
 export default function BrowseTutors() {
   const [tutorCount, setTutorCount] = useState(0);
@@ -25,7 +26,7 @@ export default function BrowseTutors() {
         title="Browse Tutors"
         rightSlot={`${tutorCount} tutor${tutorCount !== 1 ? "s" : ""} found`}
       />
-
+      {subjectLoading && <Loading bg="bg-mainBg" />}
       {!subjectLoading && (
         <SubjectTabs
           subjectTab={selectedSubject}
