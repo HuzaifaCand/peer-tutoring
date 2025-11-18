7db33d5b482f3117ed7f3696b675c25715e947b8
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-18 23:29:33 +0500

    padding

--- src/components/users/onboarding/SubmitButton.tsx
+++ src/components/users/onboarding/SubmitButton.tsx
@@ -6,7 +6,7 @@ export function SubmitButton({ isSubmitting }: { isSubmitting: boolean }) {
       <button
         type="submit"
         disabled={isSubmitting}
-        className="w-full py-3 sm:py-3.5 cursor-pointer rounded-xl bg-white/5 text-textWhite font-medium active:scale-98 transition"
+        className="w-full py-3 sm:py-3.5 cursor-pointer rounded-lg bg-white/5 text-sm sm:text-lg text-textWhite font-medium active:scale-98 transition"
       >
         {isSubmitting ? (
           <span className="flex items-center justify-center gap-2">
