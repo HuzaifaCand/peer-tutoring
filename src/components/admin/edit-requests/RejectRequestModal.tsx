bdabe0ef3998207a7856eb5974a293079577034f
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-18 11:36:29 +0500

    reject reason state now lies internally

--- src/components/admin/edit-requests/RejectRequestModal.tsx
+++ src/components/admin/edit-requests/RejectRequestModal.tsx
@@ -1,47 +1,32 @@
+"use client";
+
 import { ConfirmationModal } from "@/components/modal/ConfirmationModal";
-import { supabase } from "@/lib/supabase/client";
-import { User } from "@supabase/supabase-js";
+import { useEditRequestActions } from "./useEditRequestActions";
+import { useEffect, useState } from "react";
 
 interface Props {
-  requestModalConfig: {
-    request_id: string;
+  modalConfig: {
+    requestId: string;
     refetchTable: () => void;
     onClose: () => void;
     isOpen: boolean;
     setIsOpen: (b: boolean) => void;
-    user: User | null;
-    userLoading: boolean;
   };
-  rejectReason: string;
-  setRejectReason: (s: string) => void;
 }
 
-export function RejectRequestModal({
-  requestModalConfig,
-  setRejectReason,
-  rejectReason,
-}: Props) {
-  const {
-    request_id,
-    refetchTable,
-    onClose,
-    isOpen,
-    setIsOpen,
-    user,
-    userLoading,
-  } = requestModalConfig;
+export function RejectRequestModal({ modalConfig }: Props) {
+  const [rejectReason, setRejectReason] = useState("");
+
+  const { requestId, refetchTable, onClose, isOpen, setIsOpen } = modalConfig;
+
+  useEffect(() => {
+    if (isOpen) setRejectReason("");
+  }, [isOpen]);
+
+  const { reject } = useEditRequestActions(requestId);
 
   async function rejectRequest() {
-    if (!user || userLoading) return;
-    const { error } = await supabase
-      .from("edit_requests")
-      .update({
-        approved: false,
-        admin_id: user.id,
-        rejection_reason: rejectReason.trim(),
-      })
-      .eq("id", request_id);
-
+    const error = await reject(rejectReason);
     if (error) {
       console.error("rejecting error:", error);
       return;
