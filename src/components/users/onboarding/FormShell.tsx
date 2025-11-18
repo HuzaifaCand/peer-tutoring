1a7866c2454c85a585377abf437f17149db8bc62
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-18 17:52:05 +0500

    bg-mainBg better

--- src/components/users/onboarding/FormShell.tsx
+++ src/components/users/onboarding/FormShell.tsx
@@ -4,13 +4,13 @@ import { bg } from "../../LoginComponent";
 export function FormShell({ children }: { children: React.ReactNode }) {
   return (
     <main
-      className={`min-h-screen flex items-center justify-center px-6 ${bg}`}
+      className={`min-h-screen flex items-center justify-center px-6 bg-mainBg`}
     >
       <div
         className={clsx(
           "min-h-screen shadow-lg rounded-md p-6 lg:p-8 ",
           "w-4/5 sm:w-3/4 lg:w-2/3",
-          "border border-hoverBg bg-[#18181b] shadow-[0_0_15px_rgba(0,0,0,0.25)]"
+          "border border-hoverBg bg-mainBg shadow-[0_0_15px_rgba(0,0,0,0.25)]"
         )}
       >
         {children}
