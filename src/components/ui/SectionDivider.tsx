ebe139992b47ad094a67fb2af6ee987b257eddd9
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-18 16:30:34 +0500

    divider can be invisible now cause why not

--- src/components/ui/SectionDivider.tsx
+++ src/components/ui/SectionDivider.tsx
@@ -1,3 +1,16 @@
-export default function SectionDivider() {
-  return <div className="my-8 border-b border-textWhite/10" />;
+import clsx from "clsx";
+
+export default function SectionDivider({
+  invisible = false,
+}: {
+  invisible?: boolean;
+}) {
+  return (
+    <div
+      className={clsx(
+        "my-8",
+        invisible === true ? "invisible" : "border-b border-textWhite/10"
+      )}
+    />
+  );
 }
