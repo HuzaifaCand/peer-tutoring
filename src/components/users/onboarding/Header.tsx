6551c30edb5427dfac1f3869792d8d6b127af54b
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-18 23:23:54 +0500

    text size sorted

--- src/components/users/onboarding/Header.tsx
+++ src/components/users/onboarding/Header.tsx
@@ -1,7 +1,7 @@
 export function FormHeader({ role }: { role: "student" | "tutor" }) {
   return (
     <header>
-      <h1 className="text-2xl font-semibold text-textWhite">
+      <h1 className="text-xl sm:text-2xl font-semibold text-textWhite">
         <span className="capitalize">{role}</span> Onboarding
       </h1>
       <hr className="text-textMuted/10 mt-4 mb-6 w-full" />
