740e318231a28476c17043f7bdbf9a5b825b5b2a
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-18 11:36:13 +0500

    calls hook now

--- src/components/admin/edit-requests/ApproveRequestModal.tsx
+++ src/components/admin/edit-requests/ApproveRequestModal.tsx
@@ -1,39 +1,24 @@
+"use client";
+
 import { ConfirmationModal } from "@/components/modal/ConfirmationModal";
-import { supabase } from "@/lib/supabase/client";
-import { User } from "@supabase/supabase-js";
+import { useEditRequestActions } from "./useEditRequestActions";
 
 interface Props {
-  requestModalConfig: {
-    request_id: string;
+  modalConfig: {
+    requestId: string;
     refetchTable: () => void;
     onClose: () => void;
     isOpen: boolean;
     setIsOpen: (b: boolean) => void;
-    user: User | null;
-    userLoading: boolean;
   };
 }
-export function ApproveRequestModal({ requestModalConfig }: Props) {
-  const {
-    request_id,
-    refetchTable,
-    onClose,
-    isOpen,
-    setIsOpen,
-    user,
-    userLoading,
-  } = requestModalConfig;
+export function ApproveRequestModal({ modalConfig }: Props) {
+  const { requestId, refetchTable, onClose, isOpen, setIsOpen } = modalConfig;
+
+  const { approve } = useEditRequestActions(requestId);
 
   async function approveRequest() {
-    if (!user || userLoading) return;
-    const { error } = await supabase
-      .from("edit_requests")
-      .update({
-        approved: true,
-        admin_id: user.id,
-        rejection_reason: null,
-      })
-      .eq("id", request_id);
+    const error = await approve();
 
     if (error) {
       console.error("approving error:", error);
