235f354ca8bfe50ae9ec76c927e7ab7bd1faae1f
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-18 13:07:16 +0500

    classy refactor into profile

---
+++ src/components/users/UserInfoSection.tsx
@@ -0,0 +1,37 @@
+import { GraduationCap, Lightbulb } from "lucide-react";
+import FilledField from "../forms/fields/FilledField";
+import GradeRadioField from "./onboarding/GradeRadio";
+
+interface Props {
+  name: string;
+  studentId: string;
+  role: "tutor" | "student";
+  isOnboarding: boolean;
+}
+export function UserInfoSection({
+  name,
+  studentId,
+  role,
+  isOnboarding,
+}: Props) {
+  return (
+    <div className="flex flex-col lg:flex-row lg:gap-6 items-center lg:items-start">
+      <div className="flex items-center justify-center bg-gradient-to-l from-[#0f0f12] via-[#16161a] to-[#1b1b1f] border border-6 border-hoverBg rounded-sm w-full h-32 lg:w-50 lg:h-50 shrink-0">
+        {role === "tutor" ? (
+          <GraduationCap className="w-12 h-12 text-blue" />
+        ) : (
+          <Lightbulb className="w-12 h-12 text-blue" />
+        )}
+      </div>
+
+      <div className="flex flex-col w-full mt-6 sm:gap-1 lg:mt-0">
+        <div className="sm:flex sm:items-center sm:gap-2">
+          <FilledField label="Name" value={name} />
+          <FilledField label="ID" value={studentId} />
+        </div>
+        <FilledField label="Role" value={role} />
+        {isOnboarding && <GradeRadioField />}
+      </div>
+    </div>
+  );
+}
