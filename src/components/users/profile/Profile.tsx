0d75e0c7a3a4ae903b2d94a93a9c61d81b739b72
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-18 13:08:48 +0500

    profile page initialization

--- src/components/users/profile/Profile.tsx
+++ src/components/users/profile/Profile.tsx
@@ -1,9 +1,26 @@
+"use client";
+
 import SectionHeader from "@/components/ui/SectionHeader";
+import { UserInfoSection } from "../UserInfoSection";
+import { useAuthUser } from "@/hooks/useAuthUser";
+import Loading from "@/components/Loading";
+import { getUserMetadata } from "../getUserMetadata";
 
 export default function Profile({ role }: { role: "student" | "tutor" }) {
+  const { user, userLoading } = useAuthUser();
+
+  if (!user || userLoading) return <Loading bg="bg-mainBg" />;
+
+  const { displayName, studentId } = getUserMetadata(user);
   return (
     <main>
       <SectionHeader title="Profile" />
+      <UserInfoSection
+        name={displayName}
+        studentId={studentId}
+        role={role}
+        isOnboarding={false}
+      />
     </main>
   );
 }
