0e057050ffb6ff05c14864356d5f79c974ac6134
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-18 14:39:05 +0500

    filled field now has optional capitalization

--- src/components/forms/fields/FilledField.tsx
+++ src/components/forms/fields/FilledField.tsx
@@ -1,9 +1,16 @@
+import clsx from "clsx";
+
 interface FilledFieldProps {
   label: string;
   value: string;
+  capitalize?: boolean;
 }
 
-export default function FilledField({ label, value }: FilledFieldProps) {
+export default function FilledField({
+  label,
+  value,
+  capitalize = true,
+}: FilledFieldProps) {
   return (
     <div className="flex flex-col mb-2 w-full">
       {/* Label */}
@@ -12,7 +19,12 @@ export default function FilledField({ label, value }: FilledFieldProps) {
       </label>
 
       {/* "Input-looking" value */}
-      <div className="bg-hoverBg border capitalize border-elevatedBg text-textMuted/90 text-xs sm:text-sm rounded px-3 py-1 ">
+      <div
+        className={clsx(
+          "bg-hoverBg border border-elevatedBg text-textMuted/90 text-xs sm:text-sm rounded px-3 py-1",
+          capitalize ? "capitalize" : ""
+        )}
+      >
         {value}
       </div>
     </div>
