6dd6f65bdfcbe864ad161413efc710558e758f00
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-20 21:53:35 +0500

    search params to enable correct route

--- src/components/users/student/BrowseTutors.tsx
+++ src/components/users/student/BrowseTutors.tsx
@@ -6,17 +6,31 @@ import { useState, useEffect } from "react";
 import SubjectTabs from "./SubjectTabs";
 import { TutorCards } from "./TutorCards";
 import Loading from "@/components/Loading";
+import { useSearchParams } from "next/navigation";
 
 export default function BrowseTutors() {
   const [tutorCount, setTutorCount] = useState(0);
   const { subjects, loading: subjectLoading, error } = useUserSubjects();
   const [selectedSubject, setSelectedSubject] = useState<subject | null>(null);
 
+  const searchParams = useSearchParams();
+
   useEffect(() => {
-    if (subjects.length > 0 && !selectedSubject) {
-      setSelectedSubject(subjects[0]);
+    if (subjects.length === 0) return;
+
+    const paramId = searchParams.get("sub");
+
+    if (paramId) {
+      const match = subjects.find((s) => s.id === paramId);
+      if (match) {
+        setSelectedSubject(match);
+        return;
+      }
     }
-  }, [subjects, selectedSubject]);
+
+    // Fallback to first subject
+    setSelectedSubject(subjects[0]);
+  }, [subjects, searchParams]);
 
   if (error) console.error(error);
 
