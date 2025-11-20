4388826a29343824158dd1fd940ec392c79459be
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-20 19:53:06 +0500

    improved for mobile

--- src/components/card/EmptyCardGrid.tsx
+++ src/components/card/EmptyCardGrid.tsx
@@ -4,8 +4,8 @@ export function EmptyGrid({ text = "No results found" }: { text?: string }) {
   return (
     <div className="h-60 w-full">
       <div className="flex flex-col items-center justify-center gap-2 text-textMuted bg-mainBg border border-elevatedBg/70 rounded-md h-full">
-        <ListX className="w-12 h-12 opacity-70" />
-        <p className="text-sm font-medium text-textMuted">{text}</p>
+        <ListX className="w-10 h-10 sm:w-12 sm:h-12 opacity-70" />
+        <p className="text-xs sm:text-sm font-medium text-textMuted">{text}</p>
       </div>
     </div>
   );
--- src/components/table/EmptyTable.tsx
+++ src/components/table/EmptyTable.tsx
@@ -12,8 +12,10 @@ export function EmptyTableBody({
       <tr>
         <td colSpan={colSpan} className="h-60">
           <div className="flex flex-col items-center justify-center gap-2 text-textMuted bg-mainBg border border-elevatedBg/70 rounded-md h-full">
-            <ListX className="w-12 h-12 opacity-70" />
-            <p className="text-sm font-medium text-textMuted">{text}</p>
+            <ListX className="w-10 h-10 sm:w-12 sm:h-12 opacity-70" />
+            <p className="text-xs sm:text-sm font-medium text-textMuted">
+              {text}
+            </p>
           </div>
         </td>
       </tr>
