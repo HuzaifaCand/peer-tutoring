79265e7c168f1da4cb3cd5f9def4085156f568d9
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-20 12:27:13 +0500

    comment change

--- src/components/users/onboarding/AvailableSlots.tsx
+++ src/components/users/onboarding/AvailableSlots.tsx
@@ -95,7 +95,7 @@ export function AvailableSlots() {
   );
 }
 
-// helper function
+// helper
 export function formatHour(hour: number) {
   if (hour < 12) return `${hour}:00 AM`;
   if (hour === 12) return "12:00 PM";
