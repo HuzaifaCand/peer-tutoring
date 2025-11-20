c3e9d21d4d4d8ad3a554db831b62a67be1275e98
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-20 13:42:49 +0500

    slots area

---
+++ src/components/users/profile/TutorSlots.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useCallback } from "react";
+import { getOnsiteSlots } from "./getSlots";
+import { useDataFetch } from "@/hooks/useDataFetch";
+import { OnsiteAvailablity } from "../student/OnsiteAvailability";
+
+export function TutorSlots({ tutorId }: { tutorId: string }) {
+  const fetchFn = useCallback(() => getOnsiteSlots(tutorId), [tutorId]);
+
+  const { data: slots, loading } = useDataFetch(fetchFn);
+
+  if (!slots || loading) return null;
+  return (
+    <div className="p-4 bg-elevatedBg rounded-2xl">
+      <OnsiteAvailablity slots={slots} />
+    </div>
+  );
+}
