32903620d75f6a3bb755bf5b61c2a048cdb4f560
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-20 13:43:24 +0500

    80%

--- src/components/users/profile/Profile.tsx
+++ src/components/users/profile/Profile.tsx
@@ -5,6 +5,13 @@ import { UserInfoSection } from "../UserInfoSection";
 import { useAuthUser } from "@/hooks/useAuthUser";
 import Loading from "@/components/Loading";
 import { getUserMetadata } from "../getUserMetadata";
+import { SubjectSection } from "./SubjectSection";
+import SectionDivider from "@/components/ui/SectionDivider";
+import { TutorSlots } from "./TutorSlots";
+import { OnlineAvailabilityUI } from "../onboarding/OnlineAvailabilityUI";
+import { useState } from "react";
+import { EditRequestButton } from "./EditRequestButton";
+import { AvailabilitySection } from "./AvailabilitySection";
 
 export default function Profile({ role }: { role: "student" | "tutor" }) {
   const { user, userLoading } = useAuthUser();
@@ -24,6 +31,14 @@ export default function Profile({ role }: { role: "student" | "tutor" }) {
         isOnboarding={false}
         grade="AS" // temporary
       />
+      <SectionDivider invisible />
+      <SubjectSection user={user} role={role} />
+      {role === "tutor" && (
+        <>
+          <SectionDivider invisible />
+          <AvailabilitySection tutorId={user.id} />
+        </>
+      )}
     </main>
   );
 }
