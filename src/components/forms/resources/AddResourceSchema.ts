17a90d4440f16d6810db11b3ce0c0c9e8e38f474
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-20 20:59:05 +0500

    appropriate error

--- src/components/forms/resources/AddResourceSchema.ts
+++ src/components/forms/resources/AddResourceSchema.ts
@@ -8,7 +8,7 @@ export const addResourceSchema = z.object({
       (val) => subjectOptions.some((s) => s.id === val),
       "Please select a valid subject."
     ),
-  title: z.string().min(3),
+  title: z.string().min(3, "Title must be at least 3 characters"),
   description: z.string().max(300).optional().or(z.literal("")),
   link: z
     .string()
