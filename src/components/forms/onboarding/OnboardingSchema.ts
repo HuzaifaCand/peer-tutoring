a7e108e1ea354158e5f6574af3c96fe31a41c2ac
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-20 07:33:53 +0500

    slots schema and tutor/student schema separated

--- src/components/forms/onboarding/OnboardingSchema.ts
+++ src/components/forms/onboarding/OnboardingSchema.ts
@@ -29,16 +29,41 @@ export const getAddSubjectSchema = (role: "student" | "tutor") =>
       }),
   });
 
-export const getSlotSchema = () => {};
+// Monday - Thursday [08:00 ---- 04:00] // either i do the range which is one hour or mention the range once (1 hour slots)
 
-export const getOnboardingSchema = (role: "student" | "tutor") =>
+const VALID_DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday"] as const;
+
+export const getSlotSchema = () =>
   z.object({
-    grade: z
-      .union([z.literal(""), z.enum(["AS", "A2"])])
-      .refine((val) => val !== "", { message: "Please select a grade" }),
-    subjects: z
-      .array(getAddSubjectSchema(role))
-      .min(1, "Please select at least one subject."),
-    about: z.string().optional(),
-    role: z.literal(role),
+    day: z.enum(VALID_DAYS),
+    hour: z
+      .number()
+      .int()
+      .min(8, "Onsite slots must start at or after 8 AM")
+      .max(16, "Last slot must start before 5 PM"),
+    duration_minutes: z.number(),
   });
+
+export const studentOnboardingSchema = z.object({
+  grade: z
+    .union([z.literal(""), z.enum(["AS", "A2"])])
+    .refine((val) => val !== "", { message: "Please select a grade" }),
+  subjects: z
+    .array(getAddSubjectSchema("student"))
+    .min(1, "Please select at least one subject."),
+  about: z.string().optional(),
+  role: z.literal("student"),
+});
+
+export const tutorOnboardingSchema = z.object({
+  grade: z
+    .union([z.literal(""), z.enum(["AS", "A2"])])
+    .refine((val) => val !== "", { message: "Please select a grade" }),
+  subjects: z
+    .array(getAddSubjectSchema("tutor"))
+    .min(1, "Please select at least one subject."),
+  about: z.string().optional(),
+  slots: z.array(getSlotSchema()).min(1, "Please add at least one slot."),
+  available_online: z.boolean(),
+  role: z.literal("tutor"),
+});
