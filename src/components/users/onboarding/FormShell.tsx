1e108a3dea69ada122c513ee416c502b05f1bc95
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-20 15:59:10 +0500

    only left and right borders

--- src/components/users/onboarding/FormShell.tsx
+++ src/components/users/onboarding/FormShell.tsx
@@ -6,9 +6,9 @@ export function FormShell({ children }: { children: React.ReactNode }) {
     <main className={`min-h-screen flex items-center justify-center bg-mainBg`}>
       <div
         className={clsx(
-          "min-h-screen shadow-lg rounded-md p-6 lg:p-8 ",
+          "min-h-screen shadow-lg p-6 lg:p-8 ",
           "w-9/10 sm:w-5/6 lg:w-2/3",
-          "border border-hoverBg bg-mainBg shadow-[0_0_15px_rgba(0,0,0,0.25)]"
+          "border-l border-r border-hoverBg bg-mainBg shadow-[0_0_15px_rgba(0,0,0,0.25)]"
         )}
       >
         {children}
