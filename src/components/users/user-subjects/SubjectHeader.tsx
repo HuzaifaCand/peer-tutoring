594b593397240863643eb7c00601b30a452c0879
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-21 12:17:12 +0500

    profile specific boolean to make text on header smaller

--- src/components/users/user-subjects/SubjectHeader.tsx
+++ src/components/users/user-subjects/SubjectHeader.tsx
@@ -5,21 +5,35 @@ export function SubjectHeader({
   color,
   code,
   grade,
+  profile = false,
 }: {
   name: string;
   color: string;
   code: string;
   grade: string;
+  profile?: boolean;
 }) {
   return (
     <div className="flex items-center justify-between">
-      <h3 className={`text-xl  font-semibold text-white leading-tight`}>
+      <h3
+        className={`${
+          profile ? "text-lg" : "text-xl"
+        } text-white leading-tight font-semibold`}
+      >
         {name}
       </h3>
 
       <div className="flex items-center gap-1">
-        <Tag value={grade} color="muted" textSize="text-[12px]" />
-        <Tag value={code} color={color as colors} textSize="text-[12px]" />
+        <Tag
+          value={grade}
+          color="muted"
+          textSize="text-[10px] sm:text-[12px]"
+        />
+        <Tag
+          value={code}
+          color={color as colors}
+          textSize="text-[10px] sm:text-[12px]"
+        />
       </div>
     </div>
   );
