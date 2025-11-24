9e6f7d355f18c8d3f0bb42c004e8d641e13f1994
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-24 15:33:25 +0500

    session request modal proper

---
+++ src/components/users/browse-tutors/session-request/SessionRequestModal.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import ModalBase from "@/components/modal/ModalBase";
+import { SlotsRow } from "@/lib/computedtypes";
+import { Dispatch, SetStateAction, useState } from "react";
+import { OnsiteForm } from "./onsite/OnsiteForm";
+import { OnlineForm } from "./online/OnlineForm";
+import { ToggleModeBar } from "./ToggleMode";
+import { useAuthUser } from "@/hooks/useAuthUser";
+import { Loader, Loader2 } from "lucide-react";
+
+interface ReqModalProps {
+  reqModal: boolean;
+  setReqModal: Dispatch<SetStateAction<boolean>>;
+  slots: SlotsRow[];
+  online: boolean;
+  tutorId: string;
+  subjectId: string;
+  subjectLabel: string;
+}
+
+export function SessionRequestModal({
+  reqModal,
+  setReqModal,
+  slots,
+  online,
+  tutorId,
+  subjectId,
+  subjectLabel,
+}: ReqModalProps) {
+  const [mode, setMode] = useState<"online" | "onsite">("onsite");
+
+  const { user, userLoading } = useAuthUser();
+
+  const sharedLoad = {
+    subjectLabel,
+    subjectId,
+    tutorId,
+  };
+  return (
+    <ModalBase
+      isOpen={reqModal}
+      onClose={() => setReqModal(false)}
+      width="tight"
+    >
+      <div className="w-full flex flex-col gap-4 pt-10">
+        <ToggleModeBar online={online} setMode={setMode} mode={mode} />
+
+        {(!user || userLoading) && (
+          <div className="flex justify-center h-26">
+            <Loader2 className="w-4 h-4 animate-spin" />
+          </div>
+        )}
+        {user && mode === "onsite" && (
+          <OnsiteForm
+            sharedLoad={sharedLoad}
+            currStudentId={user.id}
+            slots={slots}
+            closeModal={() => setReqModal(false)}
+          />
+        )}
+
+        {user && mode === "online" && (
+          <OnlineForm
+            sharedLoad={sharedLoad}
+            currStudentId={user.id}
+            closeModal={() => setReqModal(false)}
+          />
+        )}
+      </div>
+    </ModalBase>
+  );
+}
