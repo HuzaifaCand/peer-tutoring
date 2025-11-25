b0b8778d73be5463a90acdabba1dd0678eeb78a8
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-26 00:47:17 +0500

    empty setup missing

---
+++ src/components/users/sessions/requests/SessionRequests.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { useCallback, useState } from "react";
+import { getSessionRequests, UnifiedRequest } from "./getSessionRequests";
+import { useDataFetch } from "@/hooks/useDataFetch";
+import { useModalOpener } from "@/components/modal/useModalOpener";
+import { useCloseModal } from "@/components/modal/useCloseModal";
+import { RequestCard } from "./RequestCard";
+import { EmptyGrid } from "@/components/card/EmptyCardGrid";
+import { defaultSorters } from "@/utils/sorters";
+import { CardsLoading } from "@/components/card/CardsLoading";
+import { SharedPropsType } from "../SessionsMain";
+import { ViewRequestModal } from "./modal/ViewRequestModal";
+
+export function SessionRequests({
+  sharedProps,
+}: {
+  sharedProps: SharedPropsType;
+}) {
+  const [selectedReq, setSelectedReq] = useState<UnifiedRequest | null>(null);
+  const { userId, role } = sharedProps;
+
+  const getSessionReqs = useCallback(
+    () => getSessionRequests(userId, role),
+    [userId]
+  );
+
+  const { data, loading, setRefetchFlag } = useDataFetch(getSessionReqs, {
+    sortFn: defaultSorters.sessionRequests,
+  });
+  const { handleOpen } = useModalOpener(setSelectedReq, "id");
+  const closeModal = useCloseModal(setSelectedReq);
+
+  if (loading)
+    return <CardsLoading count={4} layoutClassName="grid grid-cols-1 gap-3" />;
+
+  if (!data || data.length === 0)
+    return <EmptyGrid text="No Session Requests" />;
+
+  return (
+    <>
+      <ViewRequestModal
+        role={role}
+        req={selectedReq}
+        closeModal={closeModal}
+        onRefetch={() => setRefetchFlag((prev) => !prev)}
+      />
+      <div className="space-y-3 mt-3">
+        {data.map((req) => (
+          <RequestCard
+            role={role}
+            key={req.id}
+            req={req}
+            handleOpen={handleOpen}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
