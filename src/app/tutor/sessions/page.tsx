98bd05515d375f84549f8d93280d210dcca71c9b
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-26 00:53:27 +0500

    sessions

--- src/app/tutor/sessions/page.tsx
+++ src/app/tutor/sessions/page.tsx
@@ -1,3 +1,4 @@
+import SessionsMain from "@/components/users/sessions/SessionsMain";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -5,5 +6,5 @@ export const metadata: Metadata = {
 };
 
 export default function SessionsPage() {
-  return <></>;
+  return <SessionsMain role="tutor" />;
 }
