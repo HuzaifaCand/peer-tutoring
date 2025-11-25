f4826779aec718002ea5b9c09887ea3d83f62603
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-26 01:05:36 +0500

    suspense boundaries cause useModalOpener now uses useSearchParams

--- src/app/admin/edit-requests/page.tsx
+++ src/app/admin/edit-requests/page.tsx
@@ -1,9 +1,14 @@
 import EditRequests from "@/components/admin/edit-requests/EditRequests";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Resolve Edit Requests",
 };
 export default function EditRequestsPage() {
-  return <EditRequests />;
+  return (
+    <Suspense fallback={null}>
+      <EditRequests />
+    </Suspense>
+  );
 }
