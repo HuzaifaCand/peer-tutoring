258f60136211ae9516be35ab7dd6a7c6849231f3
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-26 00:48:55 +0500

    no chagnes here

--- src/components/users/sessions/SessionStats.tsx
+++ src/components/users/sessions/SessionStats.tsx
@@ -1,3 +1,13 @@
+import CountCard from "@/components/CountCard";
+
 export function SessionStats({ role }: { role: "tutor" | "student" }) {
-  return <></>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <CountCard
+        count={12}
+        label={`${role === "tutor" ? "Hours taught" : "Hours learnt"}`}
+      />
+      <CountCard count={12} label={"Sessions Completed"} />
+    </div>
+  );
 }
