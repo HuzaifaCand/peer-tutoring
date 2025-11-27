e7315f278e106638bd104f9525aec97cd113d21c
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-27 15:06:36 +0500

    final card

---
+++ src/components/users/sessions/scheduled/ScheduledSessionCard.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { CardShell } from "@/components/card/CardShell";
+import { Tag } from "@/components/ui/Tag";
+import { ScheduledSessionsType } from "./getScheduledSessions";
+import { useEffect, useState } from "react";
+import { ScheduledActions } from "./actions/ScheduledActions";
+import { SharedPropsType } from "../SessionsMain";
+import { useSessionCountdown } from "./useSessionCountdown";
+import { formatSessionCountdown } from "./formatSessionCountdown";
+import { HeaderLeft, tagTextSize } from "../sharedUI";
+
+export function ScheduledSessionCard({
+  index,
+  ss,
+  refetch,
+  sharedProps,
+}: {
+  index: number;
+  ss: ScheduledSessionsType;
+  refetch: () => void;
+  sharedProps: SharedPropsType;
+}) {
+  const { role } = sharedProps;
+  const name = role === "tutor" ? ss.sName : ss.tName;
+
+  const formatted = (ts: string | null) =>
+    ts &&
+    new Date(ts).toLocaleString("en-GB", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "2-digit",
+      hour: "numeric",
+      minute: "2-digit",
+      hour12: true,
+    });
+
+  const countdown = useSessionCountdown(ss.scheduledFor);
+  const {
+    label: countdownLabel,
+    color: countdownColor,
+    timeToSession,
+  } = formatSessionCountdown(countdown);
+
+  return (
+    <CardShell>
+      <div className="space-y-5">
+        {/* TAGS */}
+        <div className="flex items-start flex-col [@media(min-width:360px)]:flex-row [@media(min-width:360px)]:justify-between [@media(min-width:360px)]:items-center gap-2">
+          <HeaderLeft sub={ss.subjectLabel} isOnline={ss.isOnline} />
+
+          {countdownLabel && (
+            <Tag
+              textSize={tagTextSize}
+              color={index === 0 ? countdownColor : "gray"}
+              value={countdownLabel}
+            />
+          )}
+        </div>
+
+        {/* WHO + MESSAGE */}
+        <div className="space-y-3">
+          <p className="text-textWhite text-sm sm:text-lg font-medium">
+            {role === "tutor" ? "Student: " : "Tutor: "} {name}
+          </p>
+
+          {ss.message && (
+            <p className="text-[11px] sm:text-xs text-textMuted border-l-1 border-textMuted/30 pl-2">
+              {role === "student" ? "Your message: " : "Student message: "}{" "}
+              {ss.message}
+            </p>
+          )}
+
+          <hr className="h-[1px] text-textMuted/10 w-full my-4" />
+
+          <div className="flex justify-between items-center gap-3">
+            <p className="text-[10px] sm:text-[11px] text-left text-textWhite/80">
+              {formatted(ss.scheduledFor)}
+            </p>
+            <ScheduledActions
+              isOnline={ss.isOnline}
+              sessionId={ss.id}
+              timeToSession={timeToSession}
+              sharedProps={sharedProps}
+              refetch={refetch}
+            />
+          </div>
+        </div>
+      </div>
+    </CardShell>
+  );
+}
