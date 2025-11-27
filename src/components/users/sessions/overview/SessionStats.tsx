5fcaf488682c265220d87c6cd71347f315f47b49
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-27 15:09:10 +0500

    inital setup for active and overview

--- src/components/users/sessions/overview/SessionStats.tsx
+++ src/components/users/sessions/overview/SessionStats.tsx
@@ -4,8 +4,8 @@ export function SessionStats({ role }: { role: "tutor" | "student" }) {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
       <CountCard
-        count={12}
-        label={`${role === "tutor" ? "Hours taught" : "Hours learnt"}`}
+        count={700.9}
+        label={`Minutes ${role === "tutor" ? "taught" : "learnt"}`}
       />
       <CountCard count={12} label={"Sessions Completed"} />
     </div>
