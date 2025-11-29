462e36292ef4856821fee4196b7472fef4da3512
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-29 17:42:11 +0500

    remove that

--- src/components/users/user-subjects/getUserSubjects.ts
+++ src/components/users/user-subjects/getUserSubjects.ts
@@ -57,8 +57,7 @@ export async function getUserSubjects(role: "tutor" | "student", uid: string) {
   const { data: rows, error } = await supabase
     .from(table)
     .select(`subject_id, tutors!inner(approved)`)
-    .eq(idColumn, uid)
-    .eq("tutors.approved", true);
+    .eq(idColumn, uid);
 
   if (error) {
     console.error("subject ID fetch error:", error);
