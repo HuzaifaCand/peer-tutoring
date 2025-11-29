38c95cd8f5324233d0ade46dcdbfc5e115ef08c7
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-11-29 21:25:16 +0500

    card improved

--- src/components/notifications/NotificationCard.tsx
+++ src/components/notifications/NotificationCard.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Notification } from "@/lib/computedtypes";
 import { CardShell } from "../card/CardShell";
 import { Tag } from "../ui/Tag";
@@ -7,42 +5,48 @@ import { formatUnderscored } from "../admin/edit-requests/getEditRequests";
 import clsx from "clsx";
 import { formatDistanceToNow } from "date-fns";
 import Link from "next/link";
+import { CheckCheck } from "lucide-react";
 
 export function NotificationCard({ n }: { n: Notification }) {
   const content = (
     <CardShell
       className={clsx(
         "relative mb-4",
-        n.href && "cursor-pointer hover:bg-hoverBg/60 transition duration-200"
+        n.href && "cursor-pointer hover:bg-hoverBg transition duration-200"
       )}
     >
-      <div className="flex flex-col gap-4 p-0 sm:p-1">
+      <div className="flex flex-col gap-3 p-0 sm:p-1">
         {/* Header */}
-        <div className="flex justify-between items-center gap-2">
+        <div className="flex justify-between items-center gap-3">
           <div className="flex items-center gap-2">
-            {!n.read && (
-              <span className="w-2 h-2 rounded-full bg-yellow flex-shrink-0" />
-            )}
-            <h2 className="text-textWhite font-semibold text-lg sm:text-xl">
+            <h2 className="text-textWhite font-semibold text-sm sm:text-lg">
               {n.title}
             </h2>
           </div>
-
-          <Tag
-            value={formatUnderscored(n.type ?? "")}
-            color="gray"
-            textSize="text-[10px] sm:text-[12px]"
-            className="px-1 sm:px-3 py-1"
-          />
+          <div className="flex items-center gap-1">
+            <Tag
+              value={formatUnderscored(n.type ?? "")}
+              color="gray"
+              textSize="text-[10px] sm:text-[12px]"
+              className="px-0.5 sm:px-3 py-1 whitespace-nowrap"
+            />
+            <div title={!n.read ? "Unread" : "Read"}>
+              <CheckCheck
+                className={clsx(
+                  "w-3 h-3 text-blue",
+                  !n.read ? "opacity-50" : "opacity-100"
+                )}
+              />
+            </div>
+          </div>
         </div>
 
-        <div className="bg-hoverBg w-full h-[1px]" />
+        <div className="bg-hoverBg my-1 w-full h-[1px]" />
 
-        {/* Body */}
-
-        {/* Time */}
-        <div className="flex justify-between items-end gap-16">
-          <p className="text-textMuted text-xs sm:text-sm max-w-xl">{n.body}</p>
+        <div className="flex justify-between items-end gap-8">
+          <p className="text-textMuted text-[10px] sm:text-xs max-w-xl">
+            {n.body}
+          </p>
 
           <p className="text-[10px] sm:text-xs text-textMuted whitespace-nowrap">
             {formatDistanceToNow(new Date(n.created_at), {})} ago
