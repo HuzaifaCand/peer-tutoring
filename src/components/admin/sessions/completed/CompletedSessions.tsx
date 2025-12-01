7bba5f521dbac937a15dcf549db7fe628faff7bd
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-12-01 21:40:06 +0500

    unused vars

--- src/components/admin/sessions/completed/CompletedSessions.tsx
+++ src/components/admin/sessions/completed/CompletedSessions.tsx
@@ -1,13 +1,8 @@
 "use client";
 
 import SectionHeader from "@/components/ui/SectionHeader";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import CompletedSessionsTable from "./CompletedSessionsTable";
-import { ComputedCompletedSessionRow } from "../../../../lib/sessions/completed/getCompletedSessions";
-import { Modal } from "@/components/modal/ModalComponent";
-import { useCloseModal } from "@/components/modal/useCloseModal";
-import { getSessionById } from "@/lib/sessions/getSessionById";
-import { formatCompletedSession } from "@/lib/sessions/completed/formatCompletedSession";
 
 export default function CompletedSessions() {
   const [rowCount, setRowCount] = useState(0);
