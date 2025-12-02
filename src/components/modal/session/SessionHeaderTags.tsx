6272c3adb0711693f6720e5f4cf3e10c543d2da3
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-12-02 16:36:12 +0500

    minor imp

--- src/components/modal/session/SessionHeaderTags.tsx
+++ src/components/modal/session/SessionHeaderTags.tsx
@@ -10,7 +10,7 @@ export function HeaderTags({
   is_online: boolean;
 }) {
   return (
-    <header className="flex items-center gap-2 mb-5">
+    <header className="flex items-center gap-2 mb-5 pt-3.5 sm:pt-0 pb-1">
       <Tag value={subject} color="gray" font="font-medium" />
       <Tag
         value={mode}
